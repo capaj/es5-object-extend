fdbf1d135d570244b9f76506d4064968d4622397
Jiri Spac <capajj@gmail.com>
2016-01-26 01:00:35 +0000

    fixes #1 and #2

--- o.extend.js
+++ o.extend.js
@@ -1,58 +1,58 @@
-Object.defineProperty(Object, "extend", {
-	enumerable: false,
-	writable: true,
-	value: function(to, from) {
-		var props = Object.getOwnPropertyNames(from);
-		var index = props.length;
-		while(index--) {
-			var prop = props[index];
+if (!Object.hasOwnProperty('extend')) {
+  Object.defineProperty(Object, 'extend', {
+    enumerable: false,
+    writable: true,
+    value: function (to, from) {
+      var props = Object.getOwnPropertyNames(from)
+      var index = props.length
+      while (index--) {
+        var prop = props[index]
 
-			var destination;
-			if ('object' == typeof from[prop] && 'undefined' == typeof from[prop].indexOf && 'undefined' != typeof to[prop]) {
-				destination = Object.extend(to[prop], from[prop]);
-			} else {
-				destination = Object.getOwnPropertyDescriptor(from, prop);
-			}
+        var destination
+        var val = from[prop]
+        if (val === Object(val) && typeof from[prop].indexOf === 'undefined' && typeof to[prop] !== 'undefined') {
+          destination = Object.extend(to[prop], from[prop])
+        } else {
+          destination = Object.getOwnPropertyDescriptor(from, prop)
+        }
 
-			var thisProps = Object.getOwnPropertyDescriptor(to, prop);
-			if (!thisProps) {
-				thisProps = Object.getOwnPropertyDescriptor(to.__proto__, prop);
-			}
-			if (thisProps) {
-				if (destination.value && typeof thisProps.set === 'function') {
-					to[prop] = from[prop];
-				} else {
-					var descriptorProps = Object.getOwnPropertyNames(destination);
-					var descProp;
-					while(descProp = descriptorProps.pop()) {	//iterate over property descriptor and copy each property from one to
-						if (destination[descProp] !== undefined) {
-							thisProps[descProp] = destination[descProp];
-						}
-					}
-					Object.defineProperty(to, prop, thisProps);
-				}
-			} else {
-				Object.defineProperty(to, prop, destination);
-			}
+        var thisProps = Object.getOwnPropertyDescriptor(to, prop)
+        if (!thisProps) {
+          thisProps = Object.getOwnPropertyDescriptor(to.__proto__, prop)
+        }
+        if (thisProps) {
+          if (destination.value && typeof thisProps.set === 'function') {
+            to[prop] = from[prop]
+          } else {
+            var descriptorProps = Object.getOwnPropertyNames(destination)
+            var descProp
+            while(descProp = descriptorProps.pop()) {	// iterate over property descriptor and copy each property from one to
+              if (destination[descProp] !== undefined) {
+                thisProps[descProp] = destination[descProp]
+              }
+            }
+            Object.defineProperty(to, prop, thisProps)
+          }
+        } else {
+          Object.defineProperty(to, prop, destination)
+        }
+      }
+      return to
+    }
+  })
 
-		}
-
-		return to;
-	}
-});
-
-Object.defineProperty(Object, "copyOwnProperties", {	//a fast version for when you really just extend
-	enumerable: false,
-	writable: true,
-	value: function(to, from) {
-		var props = Object.getOwnPropertyNames(from);
-		var prop;
-		while(prop = props.pop()) {
-			if (from.propertyIsEnumerable(prop)) {
-				to[prop] = from[prop];
-			}
-		}
-
-		return to;
-	}
-});
+  Object.defineProperty(Object, 'copyOwnProperties', {	// a fast version for when you really just extend
+    enumerable: false,
+    writable: true,
+    value: function (to, from) {
+      var props = Object.getOwnPropertyNames(from)
+      var prop
+      while (prop = props.pop()) {
+        if (from.propertyIsEnumerable(prop)) {
+          to[prop] = from[prop]
+        }
+      }
+      return to
+    }
+  })
+}
--- test.js
+++ test.js
@@ -1,106 +1,114 @@
-require('./o.extend');
-var a = {get prop() {return [];}};
+require('./o.extend')
+var a = {get prop () { return [] }}
 
 var c = {
-	timeout: false,
-	name: 'a'
-};
+  timeout: false,
+  name: 'a'
+}
 var d = {
-	name: 'b',
-	author: 'username'
-};
+  name: 'b',
+  author: 'username'
+}
 
 module.exports = {
-	numbers: function(test) {
-		var extended = {a:10, b: 0, c: -10};
-		Object.extend(extended, {a:0, b:20, g: 0});
+  null: function (test) {
+    var b = Object.extend({}, {
+      a: null
+    })
+    test.deepEqual({
+      a: null
+    }, b)
+    test.done()
+  },
+  numbers: function (test) {
+    var extended = {a: 10, b: 0, c: -10}
+    Object.extend(extended, {a: 0, b: 20, g: 0})
 
-		test.deepEqual({a:0, b: 20, c:-10, g: 0}, extended);
-		test.done();
-	},
-	deep: function(test) {
-		var extended = {a: {b: 0, c: -10}};
-		Object.extend(extended, {a: {b:20, g: 0}});
+    test.deepEqual({a: 0, b: 20, c: -10, g: 0}, extended)
+    test.done()
+  },
+  deep: function (test) {
+    var extended = {a: {b: 0, c: -10}}
+    Object.extend(extended, {a: {b: 20, g: 0}})
 
-		test.deepEqual({a: {b: 20, c:-10, g: 0}}, extended);
-		test.done();
-	},
-	getter: function (test) {
-		var extended = Object.extend({}, a);
-		test.equals(Object.getOwnPropertyDescriptor(extended, 'prop').get.toString(), "function prop() {return [];}");
-		test.done();
-	},
-	getterOnProto: function(test) {
-		var Ct = function() {};
-		Ct.prototype = {
-			get prop() {return this._b;},
-			set prop(val){this._b = val; }
-		};
+    test.deepEqual({a: {b: 20, c: -10, g: 0}}, extended)
+    test.done()
+  },
+  getter: function (test) {
+    var extended = Object.extend({}, a)
+    test.equals(Object.getOwnPropertyDescriptor(extended, 'prop').get.toString(), 'function prop() { return [] }')
+    test.done()
+  },
+  getterOnProto: function (test) {
+    var Ct = function () {}
+    Ct.prototype = {
+      get prop () { return this._b },
+      set prop (val) { this._b = val }
+    }
 
-		var inst = new Ct();
-		Object.extend(inst, {prop: 5});
-		test.equals(inst._b, 5);
-		test.done();
-	},
-	typical: function(test) {
-		Object.extend(c, d);
-		test.equals(c.name, 'b');
-		test.equals(c.author, 'username');
-		test.done();
-	},
-	getterOverriden: function (test) {
-		var override = {get prop() {return 2;}};
-		Object.extend(a, override);
+    var inst = new Ct()
+    Object.extend(inst, {prop: 5})
+    test.equals(inst._b, 5)
+    test.done()
+  },
+  typical: function (test) {
+    Object.extend(c, d)
+    test.equals(c.name, 'b')
+    test.equals(c.author, 'username')
+    test.done()
+  },
+  getterOverriden: function (test) {
+    var override = { get prop() {return 2} }
+    Object.extend(a, override)
 
-		test.equals(a.prop, 2);
+    test.equals(a.prop, 2)
 
-		test.done();
-	},
-	setterOverriden: function (test) {
-		var base = {
-			get prop() {return this._b;},
-			set prop(val){this._b = val; },
-			_b: 3
-		};
+    test.done()
+  },
+  setterOverriden: function (test) {
+    var base = {
+      get prop () { return this._b },
+      set prop (val) { this._b = val },
+      _b: 3
+    }
 
-		var override = {set prop(val){this._c = val;}};
-		Object.extend(base, override);
-		base.prop = 6;
+    var override = {set prop (val) { this._c = val }}
+    Object.extend(base, override)
+    base.prop = 6
 
-		test.equals(base.prop, 3);
-		test.equals(base._b, 3);
-		test.equals(base._c, 6);
+    test.equals(base.prop, 3)
+    test.equals(base._b, 3)
+    test.equals(base._c, 6)
 
-		test.done();
-	},
-	valueAssignedWhenSetterExists: function (test) {
-		var base = {get prop() {return this._b;}, set prop(val){this._b = val;}};
+    test.done()
+  },
+  valueAssignedWhenSetterExists: function (test) {
+    var base = {get prop () { return this._b }, set prop (val) { this._b = val }}
 
-		var override = {prop: 5};
-		Object.extend(base, override);
+    var override = {prop: 5}
+    Object.extend(base, override)
 
-		test.equals(base._b, 5);
-		test.equals(base.prop, 5);
+    test.equals(base._b, 5)
+    test.equals(base.prop, 5)
 
-		test.done();
-	},
-	ownPropsCopy: function(test) {
-		var Ct = function() {
-			this.secondProp = 1;
-		};
-		Ct.prototype = {
-			get prop() {return this._b;},
-			set prop(val){this._b = val; }
-		};
+    test.done()
+  },
+  ownPropsCopy: function (test) {
+    var Ct = function () {
+      this.secondProp = 1
+    }
+    Ct.prototype = {
+      get prop () {return this._b},
+      set prop (val) {this._b = val}
+    }
 
-		var obj = {prop: 5};
-		Object.copyOwnProperties(obj, new Ct());
+    var obj = {prop: 5}
+    Object.copyOwnProperties(obj, new Ct())
 
-		test.equals(obj.secondProp, 1);
-		test.equals(obj.prop, 5);
-		test.equals(obj._b, undefined);
+    test.equals(obj.secondProp, 1)
+    test.equals(obj.prop, 5)
+    test.equals(obj._b, undefined)
 
-		test.done();
-
-	}
-};
+    test.done()
+  }
+}
